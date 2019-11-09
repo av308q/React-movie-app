518e53b26cdf72ed5e55a9f62bb45e315d21abe5
Angel Valdivia <angelv1901@gmail.com>
2019-11-09 16:02:18 -0500

    add style components for header and some for hero image

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,16 +1,25 @@
 import React from "react";
-import styled from "styled-components";a
+
 
 import RMDBLogo from "../images/reactMovie_logo.png";
 import TMDBLogo from "../images/tmdb_logo.svg";
 
+
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo,
+} from '../styles/StyledHeader';
+
+
+
 const Header = () => (
-	<div>
+	<StyledHeader>
 		<div className="header-content">
-			<img src={RMDBLogo} alt="rmdb-logo" />
-			<img src={TMDBLogo} alt="tmdb-logo" />
+			<StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+			<StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
 		</div>
-	</div>
+	</StyledHeader>
 );
 
 export default Header;
