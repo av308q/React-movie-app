b11f3db778672b6d8882970af0995d1bec238da8
Angel Valdivia <angelv1901@gmail.com>
2019-11-10 19:16:48 -0500

    Updated URL for hero image

--- src/config.js
+++ src/config.js
@@ -10,4 +10,7 @@ const BACKDROP_SIZE = 'w1280';
 // w92, w154, w185, w342, w500, w780, original
 const POSTER_SIZE = 'w500';
 
-export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+//added by adrianna
+const IMAGE_POSTER_URL = 'https://image.tmdb.org/t/p/original'
+
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE, IMAGE_POSTER_URL };
