2db916ae433c5d9908532341a5da695fba731fc0
Angel Valdivia <angelv1901@gmail.com>
2019-11-12 15:39:01 -0500

    Search bar is now rendering using state and callback

--- src/components/elements/SearchBar.js
+++ src/components/elements/SearchBar.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useState, useRef} from "react";
+import FontAwesome from "react-fontawesome"
 
-const SearchBar = () => <div>SearchBar</div>
+import {
+  StyledSearchBar,
+  StyledSearchBarContent
+} from '../styles/StyledSearchBar';
+
+const SearchBar = ({callback}) => {
+  const [state, setState] = useState('');
+  const timeOut = useRef(null);
+
+  const doSearch = event => {
+    console.log(event)
+  }
+
+  return (
+    <StyledSearchBar>
+      <StyledSearchBarContent>
+      <FontAwesome className="fa-search" name="search" size="2x"/>
+      <input
+        type="text"
+        placeholder="Search Movie"
+        onChange={doSearch}
+        value={state}
+        />
+
+        </StyledSearchBarContent>
+    </StyledSearchBar>
+  )
+}
 
 export default SearchBar;
