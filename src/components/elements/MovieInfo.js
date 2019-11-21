723de620851ee58a444db7d5e8030e9024ec13ba
Angel Valdivia <angelv1901@gmail.com>
2019-11-20 22:52:46 -0500

    started on movie info content and picthumb

--- src/components/elements/MovieInfo.js
+++ src/components/elements/MovieInfo.js
@@ -1,5 +1,31 @@
 import React from "react";
 
-const MovieInfo = () => <div>Movie Info</div>
+import Noimage from "../images/no_image.jpg";
+import {IMAGE_BASE_URL, POSTER_SIZE} from "../..config";
+
+import movieThumb from "./MovieThumb";
+
+import {StyleMovieInfo} from "../styles/StyledMovieInfo";
+import Movie from "../Movie";
+import MovieThumb from "./MovieThumb";
+
+
+const MovieInfo = ({movie}) => (
+  <StyleMovieInfo backdrop={movie.backdrop_path}>
+       <div className="movieinfo-content">
+       <div className="movieinfo-thumb">
+           <MovieThumb
+           image={
+             movieThumb.poster_path
+             ?`${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+             : Noimage
+           }
+           clickable={false}
+           />
+         </div>
+         </div>  
+
+  </StyleMovieInfo>
+);
 
 export default MovieInfo;
