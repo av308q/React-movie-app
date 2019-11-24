c69370898a6ba204f68f23b22088a22a7e190686
Angel Valdivia <angelv1901@gmail.com>
2019-11-24 16:44:41 -0500

    completed MovieinfoBar and actor in movie maping out api

--- src/components/elements/MovieinfoBar.js
+++ src/components/elements/MovieinfoBar.js
@@ -1,5 +1,44 @@
 import React from "react";
+import FontAwesome from "react-fontawesome";
+import { calcTime, convertMoney } from "../../helpers";
+import { StyledMovieInfoBar } from "../styles/StyledMovieInfoBar";
 
-const MovieinfoBar = () => <div>Movie Info Bar</div>
+const MovieinfoBar = ({ time, budget, revenue }) => (
+	<StyledMovieInfoBar>
+		<div className="movieinfobar-content"></div>
+			<div className="movieinfobar-content-content-col">
+				<FontAwesome className="fa-time" name="clock-o" size="2x" />
+				<span className="movieinfobar-info">
+          Running time:{calcTime(time)}
+          </span>
+			</div>
+
+      
+			<div className="movieinfobar-content-content-col">
+				<FontAwesome className="fa-budget" name="money" size="2x" />
+				<span className="movieinfobar-info">
+          Budget:{convertMoney(budget)}
+          </span>
+			</div>
+		
+
+
+    
+			<div className="movieinfobar-content-content-col">
+				<FontAwesome className="fa-revenue" name="ticket" size="2x" />
+				<span className="movieinfobar-info">
+          Running time:{convertMoney(revenue)}
+          </span>
+			</div>
+		
+
+
+
+
+
+		
+  
+	</StyledMovieInfoBar>
+);
 
 export default MovieinfoBar;
