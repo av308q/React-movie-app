0447fa6d33dd65524da607de89776d71ee959fd1
Angel Valdivia <angelv1901@gmail.com>
2019-11-24 17:13:44 -0500

    Added Persisting state to application

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { POPULAR_BASE_URL } from "../../config";
 
-export const useHomeFetch = () => {
+export const useHomeFetch = searchTerm => {
 	const [state, setState] = useState({ movies: [] });
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
@@ -34,8 +34,23 @@ export const useHomeFetch = () => {
 	};
 
 	useEffect(() => {
-		fetchMovies(POPULAR_BASE_URL);
-	}, []);
+    if (sessionStorage.homeState) {
+      setState(JSON.parse(sessionStorage));
+      setLoading(false)
+    } else {
+      console.log("Grabbing from API")
+    fetchMovies(POPULAR_BASE_URL);
+    }
+  }, []);
+  
+  useEffect(() =>{
+    if (!searchTerm) {
+      console.log("writing to sessionStorage")
+      sessionStorage.setItem('homeSate', JSON.stringify(state));
+
+
+    }
+  }, [searchTerm, state])
 
 	return [{ state, loading, error }, fetchMovies];
 };
